f14ac3e448cbb32d6a1c5211cab2f3e2953c79cf
joe2Dev <josephonwe7@gmail.com>
2024-06-10 18:41:17 +0000

    completed the Banner and Skill Scetion, modified the Navbar component and installed third party libraries to support the banner and skill components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { NavBar } from "./components/Navbar.js";
+// import { NavBar } from "./components/Navbar.js";
+import { NavBar, Banner, Skill } from "./components";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <Skill />
     </div>
   );
 }
